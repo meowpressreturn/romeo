efe709ebf1bf6bac032beab71b6d3b3a758f2f51
Andrew H <34678856+meowpressreturn@users.noreply.github.com>
2024-01-27 15:58:16 +0800

    Support arrow keys to change selection in ColorPicker
    Listen for the arrow keys and change the selected colour in the
    ColorPicker. Also listen for Escape to cancel the picker.
    
    Unfortunately the normal Swing button rendering doesn't make the
    selected button very distinct, however this commit doesn't try
    to address that problem.

--- src/main/java/romeo/ui/ColorPicker.java
+++ src/main/java/romeo/ui/ColorPicker.java
@@ -11,11 +11,17 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 import romeo.Romeo;
 import romeo.utils.Convert;
@@ -27,9 +33,10 @@ import romeo.utils.GuiUtils;
  * which it prepares on first use and then redisplays as necessary.
  */
 public class ColorPicker {
+  
   private static int ROWS = 12;
 
-  //Colours - will be arranged in vertical columns
+  //Colours - will be arranged in vertical columns of ROWS each
   private static final Color[] COLORS = new Color[] { new Color(255, 255, 255), new Color(255, 204, 102),
       new Color(255, 0, 0),
 
@@ -74,9 +81,69 @@ public class ColorPicker {
       new Color(153, 0, 204),
 
   };
+  
+  private static class ColorPickerCancelAction extends AbstractAction {
+    
+    private ColorPicker _picker;
+  
+    public ColorPickerCancelAction(ColorPicker picker) {
+      _picker = Objects.requireNonNull(picker, "picker may not be null");
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+      _picker.cancel();
+    }
+    
+  }
+  
+  private static class ColorPickerMovementAction extends AbstractAction {
+
+    public enum Direction { up, down, left, right }
+        
+    private ColorPicker _picker;
+    private Direction _direction;
+    
+    public ColorPickerMovementAction(ColorPicker picker, Direction direction) {
+      _picker = Objects.requireNonNull(picker, "picker may not be null");
+      _direction = Objects.requireNonNull(direction, "direction may not be null");
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      int i = _picker.GetSelectedIndex();
+      
+      if(i==-1) {
+        _picker.SetSelectedIndex(0);
+        return;
+      }
+      
+      int x = i / ROWS;
+      int y = i % ROWS;
+      switch(_direction) {        
+        case up: y--; break;
+        case down: y++; break; 
+        case left: x--; break;          
+        case right: x++; break;
+        default: throw new UnsupportedOperationException(_direction.toString());
+      }
+      
+      int width = (int) Math.ceil(COLORS.length / ROWS);
+      if(x>=width) x = 0;
+      if(x<0) x = width-1;
+      if(y<0) y = ROWS-1;
+      if(y>=ROWS) y = 0;
+      
+      i = y + ROWS*x;      
+      _picker.SetSelectedIndex(i);
+    }
+    
+  }
+  
+  /////////////////////////////////////////////////////////////////////////////
 
   private JDialog _dialog;
   private Color _color;
+  private List<JButton> _buttons = new ArrayList<JButton>();
 
   /**
    * Displays the picker dialog modally until the user cancels or clicks a
@@ -106,6 +173,11 @@ public class ColorPicker {
   public Color getLastColor() {
     return _color;
   }
+  
+  public void cancel() {
+    _color = null;
+    _dialog.setVisible(false);
+  }
 
   private void prepareDialog(Frame frame) {
     JDialog dialog = new JDialog(frame, "Select colour", true);
@@ -131,7 +203,6 @@ public class ColorPicker {
       String label = Convert.toStr(color);
       JButton button = new JButton(label);
       button.setBackground(color);
-      //button.setToolTipText( colorText );
       button.setFont(buttonFont);
 
       button.addActionListener(new ActionListener() {
@@ -145,6 +216,8 @@ public class ColorPicker {
       gbc.gridx = column;
       gbc.gridy = row;
       panel.add(button, gbc);
+      
+      _buttons.add(button);
 
       row++;
       if(row >= ROWS) {
@@ -154,31 +227,53 @@ public class ColorPicker {
     }
 
     JButton cancel = new JButton("Cancel");
-    cancel.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        _color = null;
-        _dialog.setVisible(false);
-      }
-    });
+    cancel.addActionListener(new ColorPickerCancelAction(this));
 
     contentPane.add(panel, BorderLayout.NORTH);
     contentPane.add(cancel, BorderLayout.SOUTH);
 
+    dialog.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("DOWN"), "DOWN");
+    dialog.getRootPane().getActionMap().put("DOWN", new ColorPickerMovementAction(this, ColorPickerMovementAction.Direction.down) );
+    
+    dialog.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("UP"), "UP");
+    dialog.getRootPane().getActionMap().put("UP", new ColorPickerMovementAction(this, ColorPickerMovementAction.Direction.up) );
+    
+    dialog.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("LEFT"), "LEFT");
+    dialog.getRootPane().getActionMap().put("LEFT", new ColorPickerMovementAction(this, ColorPickerMovementAction.Direction.left) );
+    
+    dialog.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("RIGHT"), "RIGHT");
+    dialog.getRootPane().getActionMap().put("RIGHT", new ColorPickerMovementAction(this, ColorPickerMovementAction.Direction.right) );
+    
+    dialog.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ESCAPE"), "CANCEL");
+    dialog.getRootPane().getActionMap().put("CANCEL", new ColorPickerCancelAction(this) );
+    
     dialog.pack();
     dialog.validate();
+    
     _dialog = dialog;
+  }
+  
+  private int GetSelectedIndex() {
+    for(int i=0; i<_buttons.size(); i++) {
+      if(_buttons.get(i).isFocusOwner()) return i;
+    }
+    return -1;
+  }
+  
+  private void SetSelectedIndex(int i) {
+    if(i < 0 || i >= _buttons.size()) throw new IndexOutOfBoundsException("Invalid button index " + i);
+    _buttons.get(i).requestFocus();
   }
 
   //(uncomment for quick testing while developing)
-  public static void main(String[] args) {
-    JFrame frame = new JFrame("test");
-    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    frame.setVisible(true);
-    ColorPicker p = new ColorPicker();
-    p.prepareDialog(frame);
-    p.pickColor();
-    frame.setVisible(false);
-    System.exit(0);
-  }
+//  public static void main(String[] args) {
+//    JFrame frame = new JFrame("test");
+//    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//    frame.setVisible(true);
+//    ColorPicker p = new ColorPicker();
+//    p.prepareDialog(frame);
+//    p.pickColor();
+//    frame.setVisible(false);
+//    System.exit(0);
+//  }
 }
